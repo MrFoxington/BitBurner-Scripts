3f80e7f49f54fe06ca1dd54ad77ae9eac7bf61e6
John Fox <J.Arcoudis@gmail.com>
2020-11-01 20:08:11 +1100

    Add comments to upgrade server script

--- NS2/upgradeServers.js
+++ NS2/upgradeServers.js
@@ -1,10 +1,4 @@
 export async function main(ns) {
-    // const ps = ns.ps("home");
-    // for (let i = 0; i < ps.length; ++i) {
-    //     ns.tprint(ps[i].filename + ' ' + ps[i].threads);
-    //     ns.tprint(ps[i].args);
-    // }
-
     const newRam = Math.pow(2, ns.args[0]);
     const serverList = init(ns);
     console.log(serverList);
@@ -23,7 +17,7 @@ async function upgrade(ns, newRam, serverList) {
         // console.log(ns.getServerRam(name));
 
         if (ns.getServerRam(serverList[i])[0] < newRam) {
-            let ps = ns.ps(name);
+            let ps = ns.ps(name); //Get running programs on server
             let waiting = true;
             while (waiting) {
                 if (ns.getServerMoneyAvailable('home') > ns.getPurchasedServerCost(newRam)) {
@@ -31,8 +25,8 @@ async function upgrade(ns, newRam, serverList) {
                     ns.deleteServer(name);
                     ns.purchaseServer(name, newRam);
                     if (ps.length > 0) {
-                        ns.scp(ps[0].filename, name);
-                        await ns.exec(ps[0].filename, name, 1, ps[0].args[0]);
+                        ns.scp(ps[0].filename, name); //Copy old scripts to new server
+                        await ns.exec(ps[0].filename, name, 1, ps[0].args[0]); //Restart old script (assumption that top process is main hack script)
                     }
                     waiting = false;
                 }
