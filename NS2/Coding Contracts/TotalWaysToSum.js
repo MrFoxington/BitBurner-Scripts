d316dfeceeb721ee9780377d52a3074797f6da48
John Fox <J.Arcoudis@gmail.com>
2020-11-04 23:05:11 +1100

    Add Solution Scripts Folder. Add Total-Ways-To-Sum scrupt

---
+++ NS2/Coding Contracts/TotalWaysToSum.js
@@ -0,0 +1,8 @@
+// Type: Total Ways to Sum
+// Description: It is possible write four as a sum in exactly four different ways:
+//      3 + 1
+//      2 + 2
+//      2 + 1 + 1
+//      1 + 1 + 1 + 1
+// How many different ways can the number 26 be written as a sum of at least two positive integers?
+// Data: 26
